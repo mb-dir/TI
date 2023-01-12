bd358c3956ea7017bff3833481d0d6ff3476c18f
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-01-12 12:13:28 +0100

    fixes

--- TI-project2/index.js
+++ TI-project2/index.js
@@ -3,7 +3,12 @@ const score = document.querySelector("#score");
 const scorePerSec = document.querySelector("#scorePerSec");
 const timer = document.querySelector("#timer");
 const startBtn = document.querySelector("#startBtn");
+const longest = document.querySelector("#longest");
+const shortest = document.querySelector("#shortest");
+
 let time = 10;
+
+let words = [];
 
 startBtn.addEventListener("click", () => {
   area.value = "";
@@ -12,12 +17,21 @@ startBtn.addEventListener("click", () => {
   area.focus();
   score.textContent = 0;
   scorePerSec.textContent = 0;
+  longest.textContent = "";
+  shortest.textContent = "";
   const interval = setInterval(() => {
     time -= 1;
     if (time === 0) {
       startBtn.disabled = false;
       area.disabled = true;
       time = 10;
+      //sort words by its length
+      words.sort((a, b) => b.length - a.length);
+
+      //first index === longest word, last index === shortest(after sorting)
+      longest.textContent = words[0];
+      shortest.textContent = words[words.length - 1];
+      words = [];
       clearInterval(interval);
     }
     timer.textContent = time;
@@ -26,7 +40,8 @@ startBtn.addEventListener("click", () => {
 
 area.addEventListener("input", () => {
   const trimmedValue = area.value.trim();
-  const wordsAmmount = trimmedValue.split(" ").length;
+  words = trimmedValue.split(" ");
+  const wordsAmmount = words.length;
   const wordsPerSec = (wordsAmmount / 10).toPrecision(3);
 
   score.textContent = wordsAmmount;
